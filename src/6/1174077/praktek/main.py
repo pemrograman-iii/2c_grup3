39961b1c6d9d5f187e0d8823e5d85494c8abe0a7
alvan <alvanzah99@gmail.com>
2019-04-09 23:01:03 +0700

    Alvan

---
+++ src/6/1174077/praktek/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 21:49:07 2019
+
+@author: ASUS
+"""
+
+bar = __import__('1174077_bar')
+sct = __import__('1174077_scatter')
+pie = __import__('1174077_pie')
+plt = __import__('1174077_plot')
+
+bar.balok()
+sct.sebaran()
+pie.pie_chart()
+plt.plot()
